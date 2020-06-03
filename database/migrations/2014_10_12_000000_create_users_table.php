b36065c48fc243f79d85db8eb42c28b66e27e6ec
Federico <federicoruhl@gmail.com>
2020-06-03 11:22:46 -0300

    role default and dni nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
-            $table->string('dni');
+            $table->string('dni')->nullable();
 
             $table->string('role'); //admin, patient, doctor
 
