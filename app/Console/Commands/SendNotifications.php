c80d7d9ac801a1ab7902c72e892115f9edfb2a64
Federico <federicoruhl@gmail.com>
2020-07-02 16:00:49 -0300

    Schedule command SendNotifications

---
+++ app/Console/Commands/SendNotifications.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+use App\Appointment;
+
+class SendNotifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fcm:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Enviar mensajes vía FCM';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Buscando turnos médicos con estado Confirmado en las próximas 24 horas.');
+
+        $now = Carbon::now();
+
+        $appointmentsTomorrow = $this->getAppointments24Hours($now);
+
+        foreach($appointmentsTomorrow as $appointment){
+            $appointment->patient->sendFCM('No olvides tu turno mañana a esta hora.');
+            $this->info('Mensaje FCM enviado 24 horas antes al paciente con ID: ' . $appointment->patient_id);
+        }
+
+        $appointmentsNextHour = $this->getAppointmentsNextHour($now);
+
+        foreach($appointmentsNextHour as $appointment){
+            $appointment->patient->sendFCM('Tienen un turno en una hora. Te esperamos.');
+            $this->info('Mensaje FCM enviado 1 hora antes al paciente con ID: ' . $appointment->patient_id);
+        }
+
+    }
+
+    private function getAppointments24Hours($now){
+        return Appointment::where('status', 'Confirmado')
+            ->where('scheduled_date', $now->addDay()->toDateString())
+            ->where('scheduled_time', '>=' ,$now->copy()->subMinutes(3)->toTimeString()) //usamos copy para no alterar el objeto, sino una copia del mismo
+            ->where('scheduled_time', '<' ,$now->copy()->addMinutes(2)->toTimeString()) //sumamos y restamos minutos para que sea aproximadamente a la misma hora
+            ->get();
+    }
+
+    private function getAppointmentsNextHour($now){
+        return Appointment::where('status', 'Confirmado')
+            ->where('scheduled_date', $now->addHour()->toDateString())
+            ->where('scheduled_time', '>=' ,$now->copy()->subMinutes(3)->toTimeString()) //usamos copy para no alterar el objeto, sino una copia del mismo
+            ->where('scheduled_time', '<' ,$now->copy()->addMinutes(2)->toTimeString()) //sumamos y restamos minutos para que sea aproximadamente a la misma hora
+            ->get();
+    }
+}
